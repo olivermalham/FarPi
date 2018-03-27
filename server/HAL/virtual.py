dab1c2d36ba78033ffb2ac5cb32db8792762c04c
Oliver Malham <oliver.malham@gmail.com>
2018-03-27 21:27:52 +0100

    Added a placeholder for virtual HAL components

---
+++ server/HAL/virtual.py
@@ -0,0 +1,20 @@
+from hal import HALComponent
+
+
+"""
+    Virtual components provide functionality above and beyond the basic GPIO type functions.
+"""
+
+# TODO: GroupToggle - Just toggles a group of controls between 0.0 and 1.0
+
+
+# TODO: If This, Then That - Evaluate a python expression, and if true set a state variable to a specific value
+# TODO: Combine this with the TripWire component?
+
+# TODO: TripWire - Trigger an action when a HAL state value passes a threshold
+
+
+# TODO: MQTT - A control that interacts with AWS IoT services perhaps?
+
+# TODO: WebSockets client - A control that can communicate with another FarPi instance
+
