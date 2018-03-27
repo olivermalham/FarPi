ade2c1f072caf990619de153bde19662f3dde4d2
Oliver Malham <oliver.malham@gmail.com>
2018-03-27 20:37:04 +0100

    Another bugfix

--- server/HAL/servo.py
+++ server/HAL/servo.py
@@ -21,10 +21,10 @@ class Servo(HALComponent):
 
         if self._state < (self._upper_bound - self._lower_bound) + self._lower_bound:
             hal.pi.set_servo_pulsewidth(self._pin_number, self._lower_bound)
-            self._state = self._lower_bound
+            self._state = 0.0
         else:
             hal.pi.set_servo_pulsewidth(self._pin_number, self._upper_bound)
-            self._state = self._upper_bound
+            self._state = 1.0
 
     def action_set(self, value, hal):
         self._state = value
