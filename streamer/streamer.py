e84d26e6f560b95b75c5ac8ca9abd3a4fd2e874a
om <oliver.malham@gmail.com>
2021-12-26 22:24:41 +0000

    Added note to streamer regarding video frame format

--- streamer/streamer.py
+++ streamer/streamer.py
@@ -1,10 +1,15 @@
-#Import necessary libraries
+# Import necessary libraries
 from flask import Flask, render_template, Response
 import cv2
-#Initialize the Flask app
+# Initialize the Flask app
 app = Flask(__name__)
 
+# This works with normal cameras, but not the Realsense depth stream. Need to figure out how to get hold of that data.
 camera = cv2.VideoCapture(0)
+
+# TODO: Figure out how to stream modified or generated images so that I can send enhanced video
+# Frames are ndarray objects, [y, x, colours]. All I need to do is create an image as one of these arrays,
+# then use opencv to encode it as a jpg and transmit to the browser.
 
 
 def gen_frames():
@@ -21,13 +26,15 @@ def gen_frames():
 
 @app.route('/')
 def index():
+    # Default route just to provide a simple test page
     return render_template('index.html')
 
 
 @app.route('/video_feed')
 def video_feed():
+    # Use the co-routine to generate and send image frames
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=False)  # Note: Setting debug to true causes the video capture to fail
