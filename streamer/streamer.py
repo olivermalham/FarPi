76516b4bf9b9bf8a916b98d62a4e95e4b1dc8197
om <oliver.malham@gmail.com>
2021-12-27 21:44:57 +0000

    Updated the video streamer to add an overlay on to the camera feed

--- streamer/streamer.py
+++ streamer/streamer.py
@@ -7,18 +7,18 @@ app = Flask(__name__)
 # This works with normal cameras, but not the Realsense depth stream. Need to figure out how to get hold of that data.
 camera = cv2.VideoCapture(0)
 
-# TODO: Figure out how to stream modified or generated images so that I can send enhanced video
-# Frames are ndarray objects, [y, x, colours]. All I need to do is create an image as one of these arrays,
-# then use opencv to encode it as a jpg and transmit to the browser.
-
 
 def gen_frames():
+    frame_count = 0
     while True:
+        frame_count = frame_count + 1
         success, frame = camera.read()  # read the camera frame
         if not success:
             break
         else:
+            frame = draw_overlay(frame, frame_count)
             ret, buffer = cv2.imencode('.jpg', frame)
+
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
@@ -34,6 +34,39 @@ def index():
 def video_feed():
     # Use the co-routine to generate and send image frames
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+def draw_overlay(frame, frame_count=0):
+    # Frames are ndarray objects, [y, x, blue, green, red]
+    frame = draw_crosshair(frame)
+    font = cv2.FONT_HERSHEY_PLAIN
+    fontScale = 1.2
+    color = (0, 255, 0)
+    thickness = 1
+    frame = cv2.putText(frame, f'Marvin Video Feed {frame_count}', (10, 470), font, fontScale, color, thickness, cv2.LINE_AA)
+    return frame
+
+
+def draw_crosshair(frame):
+    # Main cross hair lines
+    cv2.line(frame, (320, 0), (320, 480), (0, 255, 0), 1)
+    cv2.line(frame, (0, 240), (640, 240), (0, 255, 0), 1)
+
+    minor_tick = 5
+    major_tick = 10
+
+    # Minor ticks
+    for i in range(20, 640, 40):
+        cv2.line(frame, (i, 240-minor_tick), (i, 240+minor_tick), (0, 255, 0), 1)
+    for i in range(20, 480, 40):
+        cv2.line(frame, (320-minor_tick, i), (320+minor_tick, i), (0, 255, 0), 1)
+
+    # Major ticks
+    for i in range(40, 620, 40):
+        cv2.line(frame, (i, 240-major_tick), (i, 240+major_tick), (0, 255, 0), 1)
+    for i in range(40, 460, 40):
+        cv2.line(frame, (320-major_tick, i), (320+major_tick, i), (0, 255, 0), 1)
+    return frame
 
 
 if __name__ == "__main__":
