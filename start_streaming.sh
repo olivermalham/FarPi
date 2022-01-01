1b75166b481f4c97cb2a9730f02b706b238394bc
Oliver Malham <oliver.malham@gmail.com>
2022-01-01 15:07:39 +0000

    Updated start script to work with systemd

--- start_streaming.sh
+++ start_streaming.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+export PYTHONPATH=$PYTHONPATH:/usr/local/lib
+
 # python3 ./streamer/streamer.py 5001 1 >> ./log/camera1_stream.log 2>&1 &
 # python3 ./streamer/streamer.py 5002 2 >> ./log/camera2_stream.log overlay 2>&1 &
-python3 ./streamer_realsense/streamer_realsense.py 5000 >> ./log/realsense_stream.log 2>&1 &
+
+echo "Starting Marvin video streaming..."
+python3 ./streamer_realsense/streamer_realsense.py 5000 >> /var/log/marvin/realsense_stream.log 2>&1 
 
