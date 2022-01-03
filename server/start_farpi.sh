f88ce8aa61437256c7ec1eaf63c2bc0ce8b94b88
Oliver Malham <oliver.malham@gmail.com>
2022-01-03 18:05:21 +0000

    Rejigged start script for systemd

--- server/start_farpi.sh
+++ server/start_farpi.sh
@@ -5,6 +5,6 @@ export PYTHONPATH=$PYTHONPATH:/usr/local/lib
 # python3 ./streamer/streamer.py 5001 1 >> ./log/camera1_stream.log 2>&1 &
 # python3 ./streamer/streamer.py 5002 2 >> ./log/camera2_stream.log overlay 2>&1 &
 
-echo "Starting Marvin backend services..."
-python3 ./server/farpi.py marvin >> /var/log/marvin/farpi.log 2>&1
+echo "Starting Marvin FarPi service..."
+python3 farpi.py marvin >> /var/log/marvin/farpi.log 2>&1
 
