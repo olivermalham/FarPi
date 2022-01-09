ddc504c560adf328e0a423a244e98d01e6bd92bc
Oliver Malham <oliver.malham@gmail.com>
2022-01-09 16:10:13 +0000

    Range finder overlay working on depth stream

--- streamer_realsense/streamer_realsense.py
+++ streamer_realsense/streamer_realsense.py
@@ -19,10 +19,9 @@ pipeline_wrapper = rs.pipeline_wrapper(pipeline)
 pipeline_profile = config.resolve(pipeline_wrapper)
 
 
-def draw_overlay(frame, colour, overlay_text, origin=None):
+def draw_overlay(frame, colour, overlay_text, origin=None, font_scale=1.2):
     # Frames are ndarray objects, [y, x, blue, green, red]
     font = cv2.FONT_HERSHEY_PLAIN
-    font_scale = 1.2
     thickness = 1
     line_height = 15
 
@@ -34,17 +33,17 @@ def draw_overlay(frame, colour, overlay_text, origin=None):
 
     for line in lines:
         frame = cv2.putText(frame, line, origin, font, font_scale, colour, thickness, cv2.LINE_AA)
-        origin = origin + line_height
+        origin = (origin[0], origin[1] + line_height)
     return frame
 
 
-def draw_crosshair(frame, colour, box=0):
+def draw_crosshair(frame, colour, box=0, ticks=True):
     width = len(frame)
     height = len(frame[0])
 
     width_cen = int(width/2)
     height_cen = int(height/2)
-    half_box = box/2
+    half_box = int(box/2)
 
     # Main crosshair lines
     cv2.line(frame, (height_cen, 0), (height_cen, width_cen - half_box), colour, 1)
@@ -53,21 +52,46 @@ def draw_crosshair(frame, colour, box=0):
     cv2.line(frame, (0, width_cen), (height_cen - half_box, width_cen), colour, 1)
     cv2.line(frame, (height_cen + half_box, width_cen), (height, width_cen), colour, 1)
 
-    minor_tick = 5
-    major_tick = 10
+    if box > 0:
+        cv2.rectangle(frame, (height_cen - half_box, width_cen - half_box), (height_cen + half_box, width_cen + half_box), colour, 1)
 
-    # Minor ticks
-    for i in range(20, height, 40):
-        cv2.line(frame, (i, width_cen-minor_tick), (i, width_cen+minor_tick), colour, 1)
-    for i in range(20, width, 40):
-        cv2.line(frame, (height_cen-minor_tick, i), (height_cen+minor_tick, i), colour, 1)
+    if ticks:
+        minor_tick = 5
+        major_tick = 10
 
-    # Major ticks
-    for i in range(40, height, 40):
-        cv2.line(frame, (i, width_cen-major_tick), (i, width_cen+major_tick), colour, 1)
-    for i in range(40, width, 40):
-        cv2.line(frame, (height_cen-major_tick, i), (height_cen+major_tick, i), colour, 1)
+        # Minor ticks
+        for i in range(20, height, 40):
+            cv2.line(frame, (i, width_cen-minor_tick), (i, width_cen+minor_tick), colour, 1)
+        for i in range(20, width, 40):
+            cv2.line(frame, (height_cen-minor_tick, i), (height_cen+minor_tick, i), colour, 1)
+
+        # Major ticks
+        for i in range(40, height, 40):
+            cv2.line(frame, (i, width_cen-major_tick), (i, width_cen+major_tick), colour, 1)
+        for i in range(40, width, 40):
+            cv2.line(frame, (height_cen-major_tick, i), (height_cen+major_tick, i), colour, 1)
     return frame
+
+
+def get_range(depth_frame, box):
+    """ Return the average depth value of an area within the center of the depth frame """
+    width = depth_frame.get_width()
+    height = depth_frame.get_height()
+
+    width_cen = int(width/2)
+    height_cen = int(height/2)
+    half_box = int(box/2)
+
+    distance = 0.0  # Distance is in meters
+    count = 0  # Number of valid range pixels
+
+    for row in range(height_cen - half_box, height_cen + half_box):
+        for column in range(width_cen - half_box, width_cen + half_box):
+            new_distance = depth_frame.get_distance(column, row)
+            distance = distance + new_distance
+            count = count + 1 if new_distance > 0.0 else count
+    distance = distance / count if count > 0 else 0.0
+    return distance
 
 
 def gen_depth_frames():
@@ -77,14 +101,18 @@ def gen_depth_frames():
         # Wait for a coherent pair of frames: depth and color
         frames = pipeline.wait_for_frames()
         depth_frame = frames.get_depth_frame()
+
+        distance = get_range(depth_frame, 40)
+
         # Convert images to numpy arrays
         depth_image = np.asanyarray(depth_frame.get_data())
 
         # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
-        depth_color_mapped = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.3), cv2.COLORMAP_JET)
+        depth_color_mapped = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.1), cv2.COLORMAP_JET)
 
-        depth_color_mapped = draw_overlay(depth_color_mapped, (255, 255, 255), f"Frame No. {frame_count}")
-        depth_color_mapped = draw_crosshair(depth_color_mapped, (255, 255, 255), box=40)
+        depth_color_mapped = draw_overlay(depth_color_mapped, (0, 0, 0), f"Frame No. {frame_count}")
+        depth_color_mapped = draw_overlay(depth_color_mapped, (0, 0, 0), f"{distance:.2f}m", font_scale=1.0, origin=(int(depth_frame.get_width()/2) + 25, int(depth_frame.get_height()/2) + 15))
+        depth_color_mapped = draw_crosshair(depth_color_mapped, (0, 0, 0), box=40, ticks=False)
 
         ret, buffer = cv2.imencode('.jpg', depth_color_mapped)
 
@@ -121,7 +149,7 @@ def gen_ir_frames():
         ir_frame = frames.get_infrared_frame()
         # Convert images to numpy arrays
         ir_image = np.asanyarray(ir_frame.get_data())
-        ir_image = draw_overlay(ir_image, (255, 255, 255), f"{frame_count}")
+        ir_image = draw_overlay(ir_image, (255, 255, 255), f"Frame No. {frame_count}")
 
         ret, buffer = cv2.imencode('.jpg', ir_image)
 
@@ -159,9 +187,10 @@ if __name__ == "__main__":
     print(f"Port Number {port}")
 
     # Start streaming
-    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 10)
-    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 10)
-    config.enable_stream(rs.stream.infrared, 640, 480, rs.format.bgr8, 10)
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+    config.enable_stream(rs.stream.infrared) #, 640, 480, rs.format.bgr8, 30)
     pipeline.start(config)
+    
     
     app.run(host='0.0.0.0', port=port, debug=False)  # Note: Setting debug to true causes the video capture to fail
