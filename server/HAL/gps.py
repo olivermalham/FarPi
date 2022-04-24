16d3f15debda756cdd8e5ab59e8a1f3614e29b23
om <oliver.malham@gmail.com>
2022-04-24 21:46:49 +0100

    GPS Packet parsing working, latitude and longitude appear accurate

--- server/HAL/gps.py
+++ server/HAL/gps.py
@@ -27,65 +27,74 @@ class GPSPacket:
     mode = None
 
     @staticmethod
-    def new(*params):
-        sentence_parts = params[0]
+    def new(data):
+        data = data.strip()
+        if data is None or data == b'':
+            return None
+        sentence_parts = data.split(b",")
+        data_checksum = sentence_parts[-1].split(b"*")
+        sentence_parts[-1] = data_checksum[0]
+        sentence_parts.append(data_checksum[1])
         sentence_type = sentence_parts[0][1:]
 
         if len(sentence_type) == 0:
             return None
 
         if sentence_type == b"GPGGA":
-            return GPSPacket.GPGGA(*sentence_parts[1:])
+            return GPSPacket.GPGGA(sentence_parts[1:])
 
         if sentence_type == b"GPGLL":
-            return GPSPacket.GPGLL(*sentence_parts[1:])
+            return GPSPacket.GPGLL(sentence_parts[1:])
 
         if sentence_type == b"GPVTG":
-            return GPSPacket.GPVTG(*sentence_parts[1:])
+            return GPSPacket.GPVTG(sentence_parts[1:])
 
         if sentence_type == b"GPRMC":
-            return GPSPacket.GPRMC(*sentence_parts[1:])
+            return GPSPacket.GPRMC(sentence_parts[1:])
 
         if sentence_type == b"GPGSA":
-            return GPSPacket.GPGSA(*sentence_parts[1:])
+            return GPSPacket.GPGSA(sentence_parts[1:])
 
-        if sentence_type == b"GPGSV":
-            return GPSPacket.GPGSV(*sentence_parts[1:])
+        # TODO: Disabling for now, not sure how much I care about "satellites in view" data
+        # if sentence_type == b"GPGSV":
+        #     return GPSPacket.GPGSV(sentence_parts[1:])
 
         if sentence_type == b"GPTXT":
-            return GPSPacket.GPTXT(*sentence_parts[1:])
+            return GPSPacket.GPTXT(sentence_parts[1:])
 
     @staticmethod
-    def GPGGA(*params):
+    def GPGGA(params):
         """
         Global positioning system fix data (time, position, fix type data)
         :param params:
         :return:
         """
-        if len(params) < 13:
+        if len(params) < 15:
             print(f"GPGGA Malformed - {len(params)}: {params}")
             return None
 
         new_packet = GPSPacket()
         new_packet.time = params[0]
-        new_packet.latitude = params[1]
+        new_packet.latitude = float(params[1][:2]) + float(params[1][2:])/60 if params[1] != b'' else None
         new_packet.latitude_hemisphere = params[2]
-        new_packet.gps_fix = params[3]
-        new_packet.number_satellites = params[4]
-        new_packet.hdop = params[5]
-        new_packet.altitude = params[6]
-        new_packet.altitude_units = params[7]
-        new_packet.geoid_wgs84 = params[8]
-        new_packet.geoid_wgs84_units = params[9]
-        new_packet.time_since_dgps_update = params[10]
-        new_packet.dgps_ref_station_id = params[11]
-        new_packet.checksum = params[12]
+        new_packet.longitude = float(params[3][:3]) + float(params[3][3:])/60 if params[3] != b'' else None
+        new_packet.longitude_hemisphere = params[4]
+        new_packet.gps_fix = params[5]
+        new_packet.number_satellites = params[6]
+        new_packet.hdop = params[7]
+        new_packet.altitude = params[8]
+        new_packet.altitude_units = params[9]
+        new_packet.geoid_wgs84 = params[10]
+        new_packet.geoid_wgs84_units = params[11]
+        new_packet.time_since_dgps_update = params[12]
+        new_packet.dgps_ref_station_id = params[13]
+        new_packet.checksum = params[14]
         new_packet.packet_type = "GPGGA"
 
         return new_packet
 
     @staticmethod
-    def GPGLL(*params):
+    def GPGLL(params):
         """
         Geographic position, latitude, longitude
         :param params:
@@ -109,7 +118,7 @@ class GPSPacket:
         return new_packet
 
     @staticmethod
-    def GPVTG(*params):
+    def GPVTG(params):
         """
         Course and speed information relative to the ground
         :param params:
@@ -135,7 +144,7 @@ class GPSPacket:
         return new_packet
 
     @staticmethod
-    def GPRMC(*params):
+    def GPRMC(params):
         """
         Time, date, position, course, and speed data
         :param params:
@@ -163,7 +172,7 @@ class GPSPacket:
         return new_packet
 
     @staticmethod
-    def GPGSA(*params):
+    def GPGSA(params):
         """
         GPS receiver operating mode, satellites used in the position solution, and DOP values.
         :param params:
@@ -185,7 +194,7 @@ class GPSPacket:
         return new_packet
 
     @staticmethod
-    def GPGSV(*params):
+    def GPGSV(params):
         """
         The number of GPS satellites in view, satellite ID numbers, elevation, azimuth, and SNR values.
         :param params:
@@ -214,13 +223,38 @@ class GPSPacket:
         return new_packet
 
     @staticmethod
-    def GPTXT(*params):
+    def GPTXT(params):
         """
         Informational text?
         :param params:
         :return:
         """
         return None  # For now ignore
+    
+    def __repr__(self):
+        result = ""
+        result = result + f"Type: {self.packet_type}\n"
+        result = result + f"time: {self.time}\n"
+        result = result + f"latitude: {self.latitude}\n"
+        result = result + f"latitude_hemisphere: {self.latitude_hemisphere}\n"
+        result = result + f"longitude: {self.longitude}\n"
+        result = result + f"longitude_hemisphere: {self.longitude_hemisphere}\n"
+        result = result + f"gps_fix: {self.gps_fix}\n"
+        result = result + f"number_satellites: {self.number_satellites}\n"
+        result = result + f"hdop: {self.hdop}\n"
+        result = result + f"altitude: {self.altitude}\n"
+        result = result + f"altitude_units: {self.altitude_units}\n"
+        result = result + f"geoid_wgs84: {self.geoid_wgs84}\n"
+        result = result + f"geoid_wgs84_units: {self.geoid_wgs84_units}\n"
+        result = result + f"time_since_dgps_update: {self.time_since_dgps_update}\n"
+        result = result + f"dgps_ref_station_id: {self.dgps_ref_station_id}\n"
+        result = result + f"checksum: {self.checksum}\n"
+        result = result + f"ns_indicator: {self.ns_indicator}\n"
+        result = result + f"ew_indicator: {self.ew_indicator}\n"
+        result = result + f"utc_time: {self.utc_time}\n"
+        result = result + f"status: {self.status}\n"
+        result = result + f"mode: {self.mode}\n"
+        return result
 
 
 if __name__ == "__main__":
@@ -229,16 +263,9 @@ if __name__ == "__main__":
 
     while True:
         data = gps_serial.readline()
-        data = data.strip()
-        if data is None or data == b'':
-            continue
-        data_parts = data.split(b",")
-        data_checksum = data_parts[-1].split(b"*")
-        data_parts[-1] = data_checksum[0]
-        data_parts.append(data_checksum[1])
 
         # print(f"Data Packet: {data_parts}; Checksum: {data_checksum}")
-        packet = GPSPacket.new(data_parts)
-        if packet is not None:
-            print(f"Packet processed: {packet.packet_type}")
+        packet = GPSPacket.new(data)
+        if packet is not None and packet.packet_type == "GPGGA":
+            print(f"Packet processed: {packet}")
 
